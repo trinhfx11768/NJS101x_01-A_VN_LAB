e11960e845cc5c631bd73d3da08b7586eeb57c5e
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-16 14:42:25 +0700

    LAB1-3

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><head><title>My first page</title></head><body><h1>Hello From NodeJS!</h1></body></html>');
 });
 
 server.listen(3000);
