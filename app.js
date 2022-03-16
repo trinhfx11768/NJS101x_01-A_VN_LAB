4ce2637b6381d6d0c7c9219a12645b9655df8a23
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-16 15:01:51 +0700

    LAB1-4

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const url = req.url;
+    if(url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html><head><title>Message</title></head><body><form method="post" action="/message"><input type="text" name="message"><button type="submit">Send</button></form></body></html>');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html><head><title>My first page</title></head><body><h1>Hello From NodeJS!</h1></body></html>');
     res.end();
