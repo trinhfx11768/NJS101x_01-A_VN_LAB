eebf41a8abd4550a9eb94cabdeac50d712c1e82b
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-16 15:50:28 +0700

    LAB1-6

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
     if(url === '/') {
         res.setHeader('Content-Type', 'text/html');
         res.write('<html><head><title>Message</title></head><body><form method="post" action="/message"><input type="text" name="message"><button type="submit">Send</button></form></body></html>');
         return res.end();
     }
-    if(url === '/message') {
-        fs.writeFileSync('message.txt', 'DUMMY');
+    if(url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
         res.statusCode = 302;
         res.setHeader('Location', '/');
         return res.end();
