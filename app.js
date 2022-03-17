54f18a5a5fca64906056f96b49bb6bc1f5c06f92
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-17 08:26:18 +0700

    LAB1-7

--- app.js
+++ app.js
@@ -1,32 +1,8 @@
 const http = require('http');
-const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if(url === '/') {
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html><head><title>Message</title></head><body><form method="post" action="/message"><input type="text" name="message"><button type="submit">Send</button></form></body></html>');
-        return res.end();
-    }
-    if(url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html><head><title>My first page</title></head><body><h1>Hello From NodeJS!</h1></body></html>');
-    res.end();
-});
+const routes = require('./routes');
+
+console.log(routes.someText);
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
