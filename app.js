7d6871b3106bb510026c8975940f3ae5a94d1118
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-17 09:39:35 +0700

    LAB2-4

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
+app.use('/', (req, res, next) => {
+    console.log('This always run!');
     next();
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+    res.send(`<h1>This is "ADD PRODUCT PAGE"!</h1>`);
+});
+
+app.use('/', (req, res, next) => {
     console.log('In another middleware!');
     res.send(`<h1>Hello from Express.js</h1>`);
 });
