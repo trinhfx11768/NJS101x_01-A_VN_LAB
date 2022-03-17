7114ce6344cf2a7766dc7a0b3f6c10d5483d894c
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-17 15:45:32 +0700

    LAB2-11

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 //[GET] /admin/add-product
 router.get('/add-product', (req, res, next) => {
-    res.send(`<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>`);
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 //[POST] /admin/add-product
