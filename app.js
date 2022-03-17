f3e35e7703c7a254092c639216e4915a33b9cd03
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-17 10:34:05 +0700

    LAB2-8

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send(`<h1>Page not found</h1>`);
+})
+
 const server = http.createServer(app);
 
 server.listen(3000);
