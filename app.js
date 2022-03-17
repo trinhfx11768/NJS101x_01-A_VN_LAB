1a5522028111bb6c81f5b0b193b425fb79b8fa61
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-17 15:59:45 +0700

    LAB2-12

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const path = require('path');
 
 const express = require('express');
 
@@ -14,9 +14,8 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send(`<h1>Page not found</h1>`);
+    //path.join(__dirname,) là nơi đang sử dụng nó.
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
