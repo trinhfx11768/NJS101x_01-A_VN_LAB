390bad4bb2a8041b2bd5b9d614634b08c0ddf322
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-17 09:07:28 +0700

    LAB2-1

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const http = require('http');
 
-const routes = require('./routes');
+const express = require('express');
 
-console.log(routes.someText);
-const server = http.createServer(routes.handler);
+const app = express();
+
+const server = http.createServer(app);
 
 server.listen(3000);
