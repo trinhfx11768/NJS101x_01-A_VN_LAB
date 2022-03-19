b07e5fa18040eb03634e645a469a868901711880
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-19 21:04:56 +0700

    LAB4-2

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express = require('express');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+const errorController = require('./controllers/error');
+
 const app = express();
 
 //templates engine
@@ -33,8 +35,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {docTitle: 'Page not found', path: ''});
-})
+app.use(errorController.get404);
 
 app.listen(3000);
