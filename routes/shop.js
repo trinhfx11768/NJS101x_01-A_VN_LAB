2e65a1389a8c48289d60259ae132fd9b53258f5c
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-20 18:05:46 +0700

    LAB4-11

--- routes/shop.js
+++ routes/shop.js
@@ -11,6 +11,7 @@ router.get('/', shopController.getIndex);
 router.get('/products/:productId', shopController.getProduct);
 router.get('/products', shopController.getProducts);
 
+router.post('/cart', shopController.postCart);
 router.get('/cart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);
