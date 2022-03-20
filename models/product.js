1920879d155a396758bfe141dcb2cb28cfa77fa8
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-20 11:26:22 +0700

    LAB4-7

--- models/product.js
+++ models/product.js
@@ -1,34 +1,41 @@
 const fs = require('fs');
 const path = require('path');
-//path.dirname(process.mainModule.filename) là đường dẫn đến thư mục gốc project
-const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json');
 
-const getProductsFromFile = (cb) => {
-    fs.readFile(p, (err, fileContent) => {
-        if(err) {
-            cb([]);
-        } else {
-            cb(JSON.parse(fileContent));
-        }        
-    });    
-}
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save() {
-        getProductsFromFile(products => {
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            })
-        });
-    }
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    //static để fetchAll gọi trên chính class Product này
-    static fetchAll(cb) {
-        getProductsFromFile(cb);
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,18 @@
 const path = require('path');
 
 const express = require('express');
-const productsController = require('../controllers/products');
+
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-//[GET] /admin/add-product
-router.get('/add-product', productsController.getAddProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-//[POST] /admin/add-product
-router.post('/add-product', productsController.postAddProduct);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
