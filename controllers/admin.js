3e531f1db69f0d88a70567529ada325a051f3dbd
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-21 15:11:32 +0700

    LAB4-16

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,8 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, imageUrl, description, price);
+  //id = null để hàm save k tìm thấy id.
+  const product = new Product(null, title, imageUrl, description, price);
   product.save();
   res.redirect('/');
 };
@@ -36,6 +37,17 @@ exports.getEditProduct = (req, res, next) => {
     });
   });
 };
+
+exports.postEditProduct = (req, res) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedDesc = req.body.description;
+  const updatedPrice = req.body.price;
+  const updatedProduct = new Product(prodId, updatedTitle, updatedImageUrl, updatedDesc, updatedPrice);
+  updatedProduct.save();
+  res.redirect('/admin/products');
+}
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
