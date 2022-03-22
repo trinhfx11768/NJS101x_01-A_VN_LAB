5b981e81236a94a6926f17714670a9956a8ef55d
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-22 11:03:55 +0700

    LAB4-18

--- routes/admin.js
+++ routes/admin.js
@@ -17,5 +17,6 @@ router.post('/add-product', adminController.postAddProduct);
 // /admin/edit-product/id?edit=true => GET
 router.get('/edit-product/:productId', adminController.getEditProduct);
 router.post('/edit-product', adminController.postEditProduct);
+router.post('/delete-product', adminController.postDeleteProduct);
 
 module.exports = router;
