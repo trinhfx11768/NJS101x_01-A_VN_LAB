e4883e99b9025b96bf3f0232c9c99f557f0a3307
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-24 10:34:16 +0700

    LAB5-10

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
 
 const app = express();
 
@@ -37,4 +37,9 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync()
+    .then(result => {
+        app.listen(3000);
+    })
+    .catch(error => console.log(err));
+
