06cb6246f7f0d151d305be850f6e5b137997f31a
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-26 08:44:04 +0700

    LAB5-22

--- app.js
+++ app.js
@@ -59,7 +59,8 @@ Cart.belongsTo(User);
 Cart.belongsToMany(Product, {through: CartItem});
 Product.belongsToMany(Cart, {through: CartItem});
 
-sequelize.sync({force: true})
+sequelize.sync()
+// sequelize.sync({force: true})
     .then(user => {
         return User.findByPk(1);
     })
@@ -70,6 +71,9 @@ sequelize.sync({force: true})
         return user;
     })
     .then(user => {
+        return user.createCart();
+    })
+    .then(cart => {        
         app.listen(3000);
     })
     .catch(error => console.log(err));
