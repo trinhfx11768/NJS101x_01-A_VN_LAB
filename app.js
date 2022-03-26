a82438de2af1f588582bcfb38a1df4f2615adece
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-03-26 08:27:16 +0700

    LAB5-21

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -47,10 +49,17 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+//relationships
 Product.belongsTo(User, {constrain: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
 
-sequelize.sync()
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart, {through: CartItem});
+
+sequelize.sync({force: true})
     .then(user => {
         return User.findByPk(1);
     })
