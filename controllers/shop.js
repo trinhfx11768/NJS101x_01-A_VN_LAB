f9c97cc7b5e11947640c9ecd127e63083b84963c
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-04-01 09:49:50 +0700

    LAB5-24

--- controllers/shop.js
+++ controllers/shop.js
@@ -53,10 +53,33 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.findById(prodId, (product) => {
-    Cart.addProduct(prodId, product.price);
-  })
-  res.redirect('/cart');
+  let fetchedCart;
+  req.user.getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({where: {id: prodId}})        
+    })
+    .then(products => {
+      let product;
+      let newQuantity = 1;
+      if(products.length > 0) {
+        product = products[0]
+      }
+      if(product) {
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return fetchedCart.addProduct(product, {through: {quantity: newQuantity}});
+      }
+      return Product.findByPk(prodId)
+        .then(product => {
+          return fetchedCart.addProduct(product, {through: {quantity: newQuantity}});
+        })
+        .catch((err) => console.log(err));
+    })
+    .then(() => {
+      res.redirect('/cart');
+    })
+    .catch((err) => console.log(err));
 }
 
 exports.postCartDeleteProduct = (req, res, next) => {
