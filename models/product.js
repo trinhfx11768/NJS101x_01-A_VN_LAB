4576d1527e715ca7e4e041f45c80528e07dad299
NGUYEN HOANG TRI <trinhfx11768@funix.edu.vn>
2022-04-09 09:20:11 +0700

    LAB6-10

--- models/product.js
+++ models/product.js
@@ -3,16 +3,23 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, imageUrl, description) {
+  constructor(title, price, imageUrl, description, id) {
     this.title = title;
     this.price = price;
     this.imageUrl = imageUrl;
     this.description = description;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
-    return db.collection('products').insertOne(this)
+    let dbOp;
+    if(this._id) {
+      dbOp = db.collection('products').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: this})
+    } else {
+      dbOp = db.collection('products').insertOne(this)
+    }
+    return dbOp
       .then(result => {
         console.log(result);
       })
